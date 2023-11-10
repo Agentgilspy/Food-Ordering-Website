aa46d176c23fb37ff42bc4807eadfce039915a6e
Gilchrist <67920312+Agentgilspy@users.noreply.github.com>
2023-11-10 21:13:11 +0400

    Added Track order and create order

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const { createClient } = require('@supabase/supabase-js')
+const { randomInt } = require('crypto')
 const path = require('path')
 
+const { supabase_url, supabase_service_role } = require('./config.json')
 const port = 3000
 
 const app = express()
+const client = createClient(supabase_url, supabase_service_role)
+app.use(express.json())
 app.use(express.static('public'))
 
 
@@ -28,6 +33,22 @@ app.get('/login', (req, res) => {
 app.get('/privacy', (req, res) => {
     res.sendFile(path.join(path.join(process.cwd(), 'public', 'legal', 'privacy.html')))
 })
+app.get('/trackorder', (req, res) => {
+    res.sendFile(path.join(path.join(process.cwd(), 'public', 'user', 'trackorder.html')))
+})
+app.post('/createorder', async (req, res) => {
+    try {
+        const { cart, address, uid } = req.body
+        const order_id = randomInt(1000000, 9999999)
+        const status = 1
+        const { error } = await client.from('orders').insert({ order_id, uid, cart, status, address, })
+        if (error) throw error
+        res.status(200).json({ order_id: 10000 })
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+
 app.listen(port, () => {
     console.log('Server Running on port 3000')
 })
---
+++ public/user/trackorder.js
@@ -0,0 +1,50 @@
+const statusMessages = {
+    1: 'Order Confirmed',
+    2: 'Food is being prepared',
+    3: 'Out of delivery',
+    4: 'Delivered'
+}
+
+function createRow({ order_id, placedAt, cart, status }) {
+    // Converts the date object to a 00:00 AM time string
+    const time = new Date(placedAt).toLocaleTimeString('en-US',
+        { hour12: true, hour: '2-digit', minute: '2-digit' })
+    let cartSummary = ''
+    // Makes a Cart Summary of Quantity x ItemName
+    const items = Object.keys(cart)
+    for (const item of items) {
+        cartSummary += `${cart[item]['quantity']} x ${cart[item]['item_name']}<br>`
+    }
+    return `
+    <tr>
+    <td>${order_id}</td>
+    <td>${time}</td>
+    <td>${statusMessages[status]}</td>
+    <td>${cartSummary}</td>
+    </tr >`
+}
+window.onload = async () => {
+    try {
+        const session = await getSession()
+
+        const { data, error } = await client
+            .from('orders')
+            .select('order_id, placedAt, cart, status')
+
+        if (!data) {
+            alert('No Active Orders')
+            return location = '/menu'
+        }
+        const maintable = document.getElementById('main-table')
+        const loadingIcon = document.getElementById('loadingicon')
+        const tablebody = document.getElementById('tablebody')
+        for (const order of data) {
+            tablebody.innerHTML += createRow(order)
+        }
+        maintable.hidden = false
+        loadingIcon.remove()
+    } catch (error) {
+        alert(error.message)
+        console.error(error)
+    }
+}
